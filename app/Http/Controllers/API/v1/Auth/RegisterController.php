01974d6cbb3e964a531a55ac157112aec1ba3653
sirkenedy <kenny@adebayoadeleke.com>
2022-07-20 09:49:25 +0100

    feat: add module to extend create new administrator

--- app/Http/Controllers/API/v1/Auth/RegisterController.php
+++ app/Http/Controllers/API/v1/Auth/RegisterController.php
@@ -5,17 +5,20 @@ namespace App\Http\Controllers\API\v1\Auth;
 use App\Http\Controllers\API\BaseController;
 use App\Http\Requests\Auth\RegisterValidationRequest;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use App\Models\User;
 
 class RegisterController extends BaseController
 {
     public function index(RegisterValidationRequest $request)
     {
-        $input = $request->validated();
-        $user = User::create($input);
-        $success['token'] =  $user->createToken($request->email."-FixOnTimeAuthApp")->plainTextToken;
-        $success['name'] =  $user->name;
-   
-        return $this->handleResponse($success, 'User successfully registered!');
+        try {
+            $input = $request->validated();
+            $user = User::create($input);
+       
+            return $this->handleResponse([], 'Admin created successfully!');
+        } catch (\Throwable $th) {
+            return $this->handleError("An error occur while submitting data. please try again", [], Response::HTTP_INTERNAL_SERVER_ERROR );
+        }
     }
 }
--- app/Http/Requests/Auth/RegisterValidationRequest.php
+++ app/Http/Requests/Auth/RegisterValidationRequest.php
@@ -13,7 +13,7 @@ class RegisterValidationRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return auth('sanctum')->check();
     }
 
     /**
